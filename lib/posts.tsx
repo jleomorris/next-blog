135a002bec08ff306efcd91e7699dd42e212c90a
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-13 13:48:20 +0100

    Add getAllPostIds and getPostData to lib\posts

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -36,3 +36,46 @@ export function getSortedPostsData() {
     }
   });
 }
+
+export function getAllPostIds() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  console.log('posts.getAllPostIds.filesNames', fileNames);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data,
+  };
+}
